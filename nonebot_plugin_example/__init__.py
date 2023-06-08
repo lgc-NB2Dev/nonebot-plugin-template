62348d12ffcb9b72c4d1aaacbcc8d1851898d2d6
student_2333 <lgc2333@126.com>
2023-06-09 04:28:20 +0800

    🎉 NoneBot 2.0 🚀

--- nonebot_plugin_example/__init__.py
+++ nonebot_plugin_example/__init__.py
@@ -5,9 +5,12 @@ from .config import ConfigModel
 
 __version__ = "0.1.0"
 __plugin_meta__ = PluginMetadata(
-    "nonebot-plugin-example",
-    "插件模板",
-    "这是一个一个一个插件模板",
-    ConfigModel,
-    {"License": "MIT", "Author": "student_2333"},
+    name="nonebot-plugin-example",
+    description="插件模板",
+    usage="这是一个一个一个插件模板",
+    type="application",
+    homepage="https://github.com/lgc-NB2Dev/nonebot-plugin-example",
+    config=ConfigModel,
+    supported_adapters={"~onebot.v11"},
+    extra={"License": "MIT", "Author": "student_2333"},
 )
