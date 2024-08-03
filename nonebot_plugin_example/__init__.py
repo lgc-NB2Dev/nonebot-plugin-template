6f69aa2d6d28f5c93b87112a13ff8383c2dd14fe
student_2333 <lgc2333@126.com>
2024-08-03 18:47:21 +0800

    up

--- nonebot_plugin_example/__init__.py
+++ nonebot_plugin_example/__init__.py
@@ -1,9 +1,11 @@
+# ruff: noqa: E402
+
 from nonebot.plugin import PluginMetadata, inherit_supported_adapters, require
 
 require("nonebot_plugin_alconna")
 
-from . import __main__ as __main__  # noqa: E402
-from .config import ConfigModel  # noqa: E402
+from . import __main__ as __main__
+from .config import ConfigModel
 
 __version__ = "0.1.0"
 __plugin_meta__ = PluginMetadata(
